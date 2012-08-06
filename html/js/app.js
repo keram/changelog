39828b50a34751647ee1cc88fcf2efa9703731ce
Marek Labos <keraml@gmail.com>
2012-08-06 15:49:20 +0200

    devel progress and create guide

--- html/js/app.js
+++ html/js/app.js
@@ -8,6 +8,21 @@ $().ready(function () {
 	Changelog.prototype = {
 		source_url : '',
 
+		self_project : {
+			"title" : "Changelog",
+			"url" : "changelog.md",
+			"iframeable" : 0,
+			"web" : "",
+			"guide" : "guide.html",
+			"repository" : "https://github.com/keram/changelog/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/keram/changelog/commits/master"
+				}
+			]
+		},
+
 		getSource : function () {
 			var self = this;
 
@@ -30,7 +45,8 @@ $().ready(function () {
 			var list = '', content = '';
     	
 	    	list = $('<ul />').appendTo($('#sidebar-content'));
-	    	if (source.projects && source.projects.length > 0) {
+	    	if (source.projects) {
+				source.projects.push(this.self_project);
 				for (var i = 0, l = source.projects.length; i < l; i++) {
 		    		content += this.buildItem(source.projects[i]);
 		    	}
@@ -40,7 +56,6 @@ $().ready(function () {
 		},
 
 		setTitle : function (project_title, project_author) {
-			console.log(arguments);
 	    	var twitter_url = 'https://twitter.com/';
 	    	var author_url = twitter_url + project_author.replace('@', '');
 	    	var title = '<a href="./">' + project_title + '</a> by <a href="' + author_url + '">' + project_author + '</a>';
@@ -52,7 +67,7 @@ $().ready(function () {
 	    	var tpl = '';
 	    	tpl += '<li>';
 	    	tpl += '<div class="primary">';
-	    	tpl += '<a class="primary" href="' + data.url + '" ' + (data.iframeable ? 'iframeable' : '') + '>' + data.title + '</a>'
+	    	tpl += '<a class="primary" href="' + data.url + '" ' + (data.iframeable ? 'iframeable="1" ' : '') + '>' + data.title + '</a>'
 	    	tpl += '</div>';
 
 	    	tpl += '<div class="sub">';
@@ -70,7 +85,7 @@ $().ready(function () {
 	    	}
 
 	    	if (data.repository && data.repository.length > 0) {
-	    		tpl += '<a class="project-repository" title="' + data.title + ' repository link" href="' + data.repository + '"><img src="img/github_white_black_cat_32.png" /></a>';
+	    		tpl += '<a class="project-repository" title="' + data.title + ' repository link" href="' + data.repository + '"><img width="32" height="32" alt="Github icon" src="img/github_white_black_cat_32.png" /></a>';
 	    	}
 
 	    	tpl += '</div>';
@@ -99,6 +114,7 @@ $().ready(function () {
 		}
 	};
 
+	// smells
 	var default_source = 'http://localhost/work/keramovo/changelog/html/source.json';
     var s = document.location.search.match(/source=(http.+\.json)/);
     if (!s || s.length < 2) {
@@ -108,9 +124,37 @@ $().ready(function () {
     	var my_changelog = new Changelog(source);
 		var iframe = $('#main-iframe');
 		if (iframe.length > 0) {
-			$('#sidebar').delegate('a[iframeable]', 'click', function () {
-				var a = $(this);
-				iframe.attr('src', a.attr('href'));
+			var md_test = /\.(md|MD|txt)$/;
+			$('#sidebar').delegate('a', 'click', function (e) {
+				var a = $(this),
+					href = a.attr('href'),
+					iframeable = a.attr('iframeable');
+				e.preventDefault();
+				e.stopPropagation();					
+				iframe.hide();
+
+				if (iframeable !== 'undefined' && !!iframeable) {
+					iframe.attr('src', href);
+					iframe.show();
+				} else if (md_test.test(href)) {
+					e.preventDefault();
+					e.stopPropagation();
+					$.ajax({
+						type: 'GET',
+						url: href,
+						dataType: 'text',						
+						success: function (data) {
+							if (data) {
+								iframe.attr('src', 'data:text/plain,' + data);
+	 							iframe.show();
+							}							
+ 						}
+					});					
+
+  				} else {
+					document.location.href = href; 					
+ 				} 
+
 				return false;
 			});
 		} 		    	
