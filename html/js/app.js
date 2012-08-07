8dab4e8bccb64b5ccb750def17cfc1f4a400125f
Marek Labos <keraml@gmail.com>
2012-08-07 11:44:43 +0200

    moving to padrino

--- html/js/app.js
+++ html/js/app.js
@@ -1,165 +1,52 @@
+
+
 $().ready(function () {
 	'use strict';
-	function Changelog (source_url) {
-		this.source_url = source_url;
-		this.init();
-	}
-
-	Changelog.prototype = {
-		source_url : '',
-
-		self_project : {
-			"title" : "Changelog",
-			"url" : "changelog.md",
-			"iframeable" : 0,
-			"web" : "",
-			"guide" : "guide.html",
-			"repository" : "https://github.com/keram/changelog/",
-			"branches" : [
-				{
-					"title" : "master",
-					"url" : "https://github.com/keram/changelog/commits/master"
-				}
-			]
-		},
-
-		getSource : function () {
-			var self = this;
-
-			$.get(this.source_url, function (response) {
-		    	if (response) {
-		    		self.initSource(response);
-		    	}
-		    });    			
-		},
-
-		initSource : function (source) {
-			if (source.title && source.author) {
-	    		this.setTitle(source.title, source.author);
-	    	}
-	    	this.buildList(source);
-	    	this.bindEvents();
-		}, 
-
-		buildList : function (source) {
-			var list = '', content = '';
-    	
-	    	list = $('<ul />').appendTo($('#sidebar-content'));
-	    	if (source.projects) {
-				source.projects.push(this.self_project);
-				for (var i = 0, l = source.projects.length; i < l; i++) {
-		    		content += this.buildItem(source.projects[i]);
-		    	}
-	    	}
-	    	
-	    	list.html(content);
-		},
-
-		setTitle : function (project_title, project_author) {
-	    	var twitter_url = 'https://twitter.com/';
-	    	var author_url = twitter_url + project_author.replace('@', '');
-	    	var title = '<a href="./">' + project_title + '</a> by <a href="' + author_url + '">' + project_author + '</a>';
-	    	$('#logo').html(title);
-		},
-
-		buildItem : function (data) {
-	    	var item = 'item';
-	    	var tpl = '';
-	    	tpl += '<li>';
-	    	tpl += '<div class="primary">';
-	    	tpl += '<a class="primary" href="' + data.url + '" ' + (data.iframeable ? 'iframeable="1" ' : '') + '>' + data.title + '</a>'
-	    	tpl += '</div>';
-
-	    	tpl += '<div class="sub">';
-
-	    	if (data.web && data.web.length > 0) {
-	    		tpl += '<a class="project-web" title="' + data.title + ' website" href="' + data.web + '">W</a>';
-	    	}
-
-	    	if (data.guide && data.guide.length > 0) {
-	    		tpl += '<a class="project-guide" title="' + data.title + ' guides" href="' + data.guide + '">G</a>';
-	    	}
-
-	    	if (data.download && data.download.length > 0) {
-	    		tpl += '<a class="project-download" title="' + data.title + ' download link, page or instalation instructions" href="' + data.download + '">D</a>';
-	    	}
-
-	    	if (data.repository && data.repository.length > 0) {
-	    		tpl += '<a class="project-repository" title="' + data.title + ' repository link" href="' + data.repository + '">R</a>';
-	    	}
-
-	    	tpl += '</div>';
-	    	
-	    	tpl += '<ul>';
-	    	if (data.branches && data.branches.length > 0) {
-	    		var branch = null;
-	    		for (var j = 0; j < data.branches.length; j++ ) {
-	    			branch = data.branches[j];
-	    			tpl += '<li><a href="' + branch.url + '">' + branch.title + '</a></li>';
-	    		}
-	    	}
-	    	tpl += '</ul>';    	
-
-	    	tpl += '</li>';
-	    	item = tpl;
-	    	return item;
-		},
-
-		bindEvents : function () {
-	    	
-		},
-
-		init : function () {
-			this.getSource();
-		}
-	};
 
 	// smells
-	var default_source = 'http://localhost/work/keramovo/changelog/html/source.json';
-    var s = document.location.search.match(/source=(http.+\.json)/);
-    if (!s || s.length < 2) {
-    	document.location.href = document.location.href + '?source=' + default_source;
-    } else {
-		var source = s[1];
-    	var my_changelog = new Changelog(source);
-		var iframe = $('#main-iframe');
-		if (iframe.length > 0) {
-			var md_test = /\.(md|MD|txt)$/;
-			$('#sidebar').delegate('a', 'click', function (e) {
-				var a = $(this),
-					href = a.attr('href'),
-					iframeable = a.attr('iframeable');
+	var iframe = $('#main-iframe');
+	if (iframe.length > 0) {
+		var md_test = /\.(md|MD|txt)$/,
+			proxy_host = 'http://localhost/',
+			proxy_url = '';
+		$('#sidebar').delegate('a', 'click', function (e) {
+			var a = $(this),
+				href = a.attr('href'),
+				is_primary = a.hasClass('primary');
+			e.preventDefault();
+			e.stopPropagation();					
+			iframe.hide();
+
+			if (is_primary) {
+				console.log('is primary');
+				proxy_url = proxy_host + '?url=' + href;
+				console.log(proxy_url);
+				//iframe.attr('src', href);
+				// iframe.show();
+			} else if (md_test.test(href)) {
 				e.preventDefault();
-				e.stopPropagation();					
-				iframe.hide();
+				e.stopPropagation();
+				$.ajax({
+					type: 'GET',
+					url: href,
+					dataType: 'text',
+					error: function (r) {
+						console.log(r);
+					},
+					success: function (data) {
+						if (data) {
+							iframe.attr('src', 'data:text/plain,' + data);
+ 							iframe.show();
+						}							
+					}
+				});					
 
-				if (iframeable !== 'undefined' && !!iframeable) {
-					iframe.attr('src', href);
-					iframe.show();
-				} else if (md_test.test(href)) {
-					e.preventDefault();
-					e.stopPropagation();
-					$.ajax({
-						type: 'GET',
-						url: href,
-						dataType: 'text',
-						error: function (r) {
-							console.log(r);
-						},
-						success: function (data) {
-							if (data) {
-								iframe.attr('src', 'data:text/plain,' + data);
-	 							iframe.show();
-							}							
- 						}
-					});					
+			} else {
+				document.location.href = href; 					
+			} 
 
-  				} else {
-					document.location.href = href; 					
- 				} 
+			return false;
+		});
+	} 		    	
 
-				return false;
-			});
-		} 		    	
-	}
 });
---
+++ html/source.js
@@ -0,0 +1,96 @@
+initChangelog({
+	"author" : "@marek_public",
+	"title" : "Dev Changelog",
+	"projects" : [
+		{
+			"title" : "Ruby on Rails",
+			"url" : "http://weblog.rubyonrails.org/",
+			"iframeable" : 1,
+			"web" : "http://rubyonrails.org/",
+			"guide" : "http://guides.rubyonrails.org/",
+			"download" : "http://rubyonrails.org/download",
+			"repository" : "https://github.com/rails/rails/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/rails/rails/commits/master"
+				},
+				{
+					"title" : "3-2-stable",
+					"url" : "https://github.com/rails/rails/commits/3-2-stable"
+				}
+			]
+		}, {
+			"title" : "Node.js",
+			"url" : "http://nodejs.org/changelog.html",
+			"iframeable" : 1,
+			"web" : "http://nodejs.org/",
+			"guide" : "http://guides.rubyonrails.org/",
+			"download" : "http://rubyonrails.org/download",
+			"repository" : "https://github.com/joyent/node/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/joyent/node/commits/master"
+				}
+			]
+		}, {
+			"title" : "Nginx",
+			"url" : "http://nginx.org/en/CHANGES",
+			"iframeable" : 1,
+			"web" : "http://nginx.org/en/",
+			"repository" : "https://github.com/git-mirror/nginx/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/git-mirror/nginx/commits/master"
+				}
+			]
+		}, {
+			"title" : "jQuery",
+			"url" : "http://blog.jquery.com/",
+			"iframeable" : 1,
+			"web" : "http://jquery.com/",
+			"repository" : "https://github.com/jquery/jquery/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/jquery/jquery/commits/master"
+				}
+			]
+		}, {
+			"title" : "AngularJS",
+			"url" : "https://raw.github.com/angular/angular.js/master/CHANGELOG.md",
+			"iframeable" : 0,
+			"web" : "http://angularjs.org/",
+			"repository" : "https://github.com/angular/angular.js/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/angular/angular.js/commits/master"
+				}
+			]
+		}, {
+			"title" : "PHP",
+			"url" : "http://php.net/ChangeLog-5.php",
+			"iframeable" : 1,
+			"web" : "http://php.net/"
+		}, {
+			"title" : "RefineryCMS",
+			"url" : "https://raw.github.com/resolve/refinerycms/master/changelog.md",
+			"iframeable" : 0,
+			"web" : "http://refinerycms.com/",
+			"repository" : "https://github.com/resolve/refinerycms/",
+			"branches" : [
+				{
+					"title" : "master",
+					"url" : "https://github.com/resolve/refinerycms/commits/master"
+				},
+				{
+					"title" : "2-0-stable",
+					"url" : "https://github.com/resolve/refinerycms/commits/2-0-stable"
+				}				
+			]
+		}
+	]
+});
